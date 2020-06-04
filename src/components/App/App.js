a2c5cab29539f0a6bdab1d66e36a64478dfc81c0
Caruy <caruychen@gmail.com>
2020-06-04 15:14:17 +0300

    implemented playlist name update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -51,6 +52,12 @@ class App extends React.Component {
     });
   }
 
+  updatePlaylistName(name) {
+    this.setState({
+      playlistName: name
+    })
+  }
+
   render() {
     return (
       <div>
@@ -62,7 +69,8 @@ class App extends React.Component {
               onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} 
               playlistTracks={this.state.playlistTracks} 
-              onRemove={this.removeTrack}/>
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}/>
           </div>
         </div>
       </div>
